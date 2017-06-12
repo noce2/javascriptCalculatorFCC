b19282d9cdc964645ba5b61fe7691ad7e62852e8
Nsikan <nsikane@live.co.uk>
2017-06-12 23:41:25 +0100

    Configured Webpack Tests & Fixed Chrome Tab Autoclose issue
    For fix to auto close, I changed the singlewatch parameter in karma config. Also modified package.json to include autowatch in test commands so that test can rerun as I work.
    
    Configured Webpack Tests:
    I also modified the typescript loader config so that it used my file in /src.
    
    Also deleted system.js files because i don't need them.

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -12,12 +12,22 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
+        /*
         loaders: [
           {
             loader: 'awesome-typescript-loader',
             options: { configFileName: helpers.root('src', 'tsconfig.json') }
           } , 'angular2-template-loader'
         ]
+        */
+        use: [{
+          loader: 'awesome-typescript-loader',
+          options: {
+            configFileName: './src/tsconfig.json'
+          }
+        }, {
+          loader: 'angular2-template-loader'
+        }]
       },
       {
         test: /\.html$/,
